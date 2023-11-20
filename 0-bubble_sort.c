486ba61644a3f02e49c5803718087a3130532a8d
Zvikomboreronyambuya <znyambuya@gmail.com>
2023-11-20 01:13:55 -0800

    corrected a small issue in the inner loop condition if (array[index] > array[index + 1] && array[index + 1])

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,20 +7,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-
-	size_t n, index, tmp = 0;
+	size_t n, index;
+	int tmp = 0;
 
 	if (size < 2)
 		return;
+
 	for (n = 0; n < size; n++)
-		for (index = 0; index < size; index++)
+		for (index = 0; index < size - 1; index++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[index] > array[index + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				tmp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = tmp;
+				print_array(array, size);
 			}
 		}
 }
+
